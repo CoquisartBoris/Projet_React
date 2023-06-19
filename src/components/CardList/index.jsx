1f01d988af2dbe9ac44a4bda4307c925128ba604
Coquisart Boris <coquisartb@gmail.com>
2023-06-19 16:40:33 +0200

    Page d'acceuil

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -4,6 +4,7 @@ import MyCard from '../../components/MyCard'
 
 function CardList() {
     const [datas, setDatas] = useState(null)
+    const i = 0
     
     useEffect(() => {
         fetch('/Data/data.json').then((response) => {
@@ -14,9 +15,13 @@ function CardList() {
     },[])
 
     return (
-        
         <div className='cardWrapper'>
-            <MyCard data={datas}/>
+            <MyCard data={datas[0]}/>
+            <MyCard data={datas[1]}/>
+            <MyCard data={datas[2]}/>
+            <MyCard data={datas[3]}/>
+            <MyCard data={datas[4]}/>
+            <MyCard data={datas[5]}/>
         </div>
     )
 }
--- src/components/MyCard/index.jsx
+++ src/components/MyCard/index.jsx
@@ -2,9 +2,14 @@ import './myCard.scss'
 
 function myCard(props) {
     console.log(props)
-    return [
-        <h1>{props.data[1].title}</h1>
-    ]
+    return (
+        
+        <div className='card'>
+            <img src={props.data.cover} className='cardImg'></img>
+            <h1 className='cardTitle'>{props.data.title}</h1>
+        </div>
+        
+    )
 }
 
 export default myCard
