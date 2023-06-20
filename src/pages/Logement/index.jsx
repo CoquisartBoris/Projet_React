377793d3e8c1f3fbbc3db85020759f6193483060
Coquisart Boris <coquisartb@gmail.com>
2023-06-20 12:01:58 +0200

    maj page logement 50%

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -4,12 +4,18 @@ import './logement.scss'
 function Logement() {
     const location = useLocation()
     const { newData } = location.state
-    //console.log(newData.props.data.title)
+    const tags = newData.props.data.tags
+    const tagList = tags.map((tag) =>
+        <li>{tag}</li>
+    );
 
     return (
         <div className='logementWrapper'>
-            <img src={newData.props.data.cover} className='logementImg'></img>
-            <h1>{newData.props.data.title}</h1>
+            <h1 className='logementTitle'>{newData.props.data.title}</h1>
+            <p className='logementHost'>{newData.props.data.host.name}</p>
+            <img src={newData.props.data.host.picture}></img>
+            <p className='logementLocation'>{newData.props.data.location}</p>
+            <ul className='logementTags'>{tagList}</ul>
         </div>
     )
 }
