5c7d1e2ff2cdcc27b81cce1ecefd8fdc671c1890
Coquisart Boris <coquisartb@gmail.com>
2023-06-24 10:37:53 +0200

    maj media query responsiv mobile

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -4,7 +4,6 @@ import MyCard from '../../components/MyCard'
 
 function CardList() {
     const [datas, setDatas] = useState(null)
-    let i = 0;
     
     useEffect(() => {
         fetch('/Data/data.json').then((response) => {
@@ -17,18 +16,7 @@ function CardList() {
     const listItems = datas.map((test) =>
         <MyCard data={test}/>
     )
-    /*
-    return (
-        <div className='cardWrapper'>
-            <MyCard data={datas[0]}/>
-            <MyCard data={datas[1]}/>
-            <MyCard data={datas[2]}/>
-            <MyCard data={datas[3]}/>
-            <MyCard data={datas[4]}/>
-            <MyCard data={datas[5]}/>
-        </div>
-    )
-    */
+    
    return (
     <ul className='cardWrapper'>{listItems}</ul>
    )
