fba606b57af0c612d5a38d6a8dee3c27a3cfac19
Coquisart Boris <coquisartb@gmail.com>
2023-07-13 10:46:56 +0200

    maj 48H : maj Cards liens text-decoration

--- src/components/MyCard/index.jsx
+++ src/components/MyCard/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function myCard({title, cover, id}) {
     const path = "Logement/" + id
     return (
-        <Link to={path}>
+        <Link className='cardLink' to={path}>
             <div className='card'>
                 <img src={cover} className='cardImg' alt={title}></img>
                 <h1 className='cardTitle'>{title}</h1>
